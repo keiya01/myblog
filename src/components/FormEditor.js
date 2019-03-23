6d0f45c041f45124e7e433b2494fe6e92b1a23b7
keiya01 <keiya.s.0210@gmail.com>
2019-03-23 17:06:01 +0900

    Add new line function

--- src/components/FormEditor.js
+++ src/components/FormEditor.js
@@ -2,38 +2,54 @@ import React from "react";
 import { StyleSheet, css } from 'aphrodite';
 import { Editor, EditorState, RichUtils } from "draft-js";
 
-const { useState } = React;
+const { useState, useRef } = React;
 
 export default function FormEditor(props) {
     const [editorState, changeEditorState] = useState(EditorState.createEmpty());
+    const editor = useRef(null)
+
+    const handleOnFocusEditor = () => {
+        editor.current.focus();
+    }
 
     const keyBindingFn = e => {
         switch (e.keyCode) {
             case 32 /* this code is Shift */: {
-                return "myList"
+                return "insertList";
+            }
+            case 13: {
+                return "insertNewLine"
             }
             default: {
-                return
+                return;
             }
         }
     }
 
     const handleKeyCommand = (command, editorState) => {
-        if (command === "myList") {
-            const newState = RichUtils.toggleBlockType(editorState, 'unordered-list-item');
-            changeEditorState(newState);
-            return 'handled';
+        switch (command) {
+            case "insertList": {
+                changeEditorState(RichUtils.toggleBlockType(editorState, 'unordered-list-item'));
+                return 'handled';
+            }
+            case "insertNewLine": {
+                changeEditorState(RichUtils.insertSoftNewline(editorState));
+                return "handle"
+            }
+            default: {
+                return 'not-handled';
+            }
         }
-        return 'not-handled';
     }
 
     return (
-        <div className={css(styles.container)}>
+        <div className={css(styles.container)} onClick={handleOnFocusEditor}>
             <Editor
                 editorState={editorState}
                 onChange={e => changeEditorState(e)}
                 handleKeyCommand={handleKeyCommand}
-                keyBindingFn={keyBindingFn} />
+                keyBindingFn={keyBindingFn}
+                ref={editor} />
         </div>
     );
 }
@@ -41,8 +57,10 @@ export default function FormEditor(props) {
 const styles = StyleSheet.create({
     container: {
         width: "90%",
-        maxWidth: 480,
+        maxWidth: 600,
         height: "100%",
         minHeight: "100vh",
+        border: "1px solid #222",
+        margin: "0 auto",
     }
 })
